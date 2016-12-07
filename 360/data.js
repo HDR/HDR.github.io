2849222ceaf754b271226031cdd23f47cd237c48
Martin <refshet@yahoo.no>
2016-12-07 01:22:51 +0100

    Update data.js

--- 360/data.js
+++ 360/data.js
@@ -279,6 +279,37 @@ var APP_DATA = {
       "linkHotspots": [],
       "infoHotspots": []
     }
+        {
+      "id": "0-the-witcher-3-360-20161207---01154676",
+      "name": "The Witcher 3 360 2016.12.07 - 01.15.46.76",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
   ],
   "name": "Witcher 3",
   "settings": {
