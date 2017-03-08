62b383670b61bc8ffff705b2cf3ab39da1c98fc4
MrHDR <refshet@yahoo.no>
2017-03-08 17:01:16 +0100

    Added non beta 360 stuff

--- 360/GRW/data.js
+++ 360/GRW/data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "8-tom-clancys-ghost-recon--wildlands-360-9",
-      "name": "Tom Clancy's Ghost Recon  Wildlands 360 1",
+      "id": "0-tom-clancys-ghost-recon--wildlands-1",
+      "name": "Tom Clancy's Ghost Recon  Wildlands 1",
       "levels": [
         {
           "tileSize": 256,
@@ -24,47 +24,16 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": 0.05258276950832652,
-        "pitch": 0.08382556349546277,
+        "yaw": -3.0471363690750177,
+        "pitch": 0.15823418227897612,
         "fov": 1.5707963267948966
       },
       "linkHotspots": [],
       "infoHotspots": []
     },
     {
-      "id": "0-tom-clancys-ghost-recon--wildlands-360-1",
-      "name": "Tom Clancy's Ghost Recon  Wildlands 360 2",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "yaw": -0.6693271435378065,
-        "pitch": 0.34076038614933424,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
-    },
-    {
-      "id": "1-tom-clancys-ghost-recon--wildlands-360-2",
-      "name": "Tom Clancy's Ghost Recon  Wildlands 360 3",
+      "id": "1-tom-clancys-ghost-recon--wildlands-2",
+      "name": "Tom Clancy's Ghost Recon  Wildlands 2",
       "levels": [
         {
           "tileSize": 256,
@@ -94,8 +63,39 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "2-tom-clancys-ghost-recon--wildlands-360-3",
-      "name": "Tom Clancy's Ghost Recon  Wildlands 360 4",
+      "id": "2-tom-clancys-ghost-recon--wildlands-3",
+      "name": "Tom Clancy's Ghost Recon  Wildlands 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.0081066304905022,
+        "pitch": 0.17061772263326702,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-tom-clancys-ghost-recon--wildlands-3",
+      "name": "Tom Clancy's Ghost Recon  Wildlands 3",
       "levels": [
         {
           "tileSize": 256,
@@ -125,8 +125,39 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "3-tom-clancys-ghost-recon--wildlands-360-4",
-      "name": "Tom Clancy's Ghost Recon  Wildlands 360 5",
+      "id": "4-tom-clancys-ghost-recon--wildlands-4",
+      "name": "Tom Clancy's Ghost Recon  Wildlands 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.2556519834431725,
+        "pitch": 0.08541888923113916,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-tom-clancys-ghost-recon--wildlands-5",
+      "name": "Tom Clancy's Ghost Recon  Wildlands 5",
       "levels": [
         {
           "tileSize": 256,
@@ -156,8 +187,8 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "4-tom-clancys-ghost-recon--wildlands-360-5",
-      "name": "Tom Clancy's Ghost Recon  Wildlands 360 6",
+      "id": "6-tom-clancys-ghost-recon--wildlands-6",
+      "name": "Tom Clancy's Ghost Recon  Wildlands 6",
       "levels": [
         {
           "tileSize": 256,
@@ -187,70 +218,8 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "5-tom-clancys-ghost-recon--wildlands-360-6",
-      "name": "Tom Clancy's Ghost Recon  Wildlands 360 7",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
-    },
-    {
-      "id": "6-tom-clancys-ghost-recon--wildlands-360-7",
-      "name": "Tom Clancy's Ghost Recon  Wildlands 360 8",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
-    },
-    {
-      "id": "7-tom-clancys-ghost-recon--wildlands-360-8",
-      "name": "Tom Clancy's Ghost Recon  Wildlands 360 9",
+      "id": "7-tom-clancys-ghost-recon--wildlands-7",
+      "name": "Tom Clancy's Ghost Recon  Wildlands 7",
       "levels": [
         {
           "tileSize": 256,
@@ -284,7 +253,7 @@ var APP_DATA = {
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": false,
-    "fullscreenButton": true,
+    "fullscreenButton": false,
     "viewControlButtons": false
   }
 };
