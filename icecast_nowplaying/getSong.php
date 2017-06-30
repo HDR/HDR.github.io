cf3bd661ecd0042dfea8d72ecce4e8c67b5549f5
Martin <MrHDR@users.noreply.github.com>
2017-06-30 14:31:45 +0200

    Create getSong.php

---
+++ icecast_nowplaying/getSong.php
@@ -0,0 +1,36 @@
+<?php
+	/**
+	 * Please be aware. This gist requires at least PHP 5.4 to run correctly.
+	 * Otherwise consider downgrading the $opts array code to the classic "array" syntax.
+	 */
+	function getMp3StreamTitle($streamingUrl, $interval, $offset = 0, $headers = true)
+	{
+		$needle = 'StreamTitle=';
+		$ua = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.110 Safari/537.36';
+		$opts = [
+			'http' => [
+				'method' => 'GET',
+				'header' => 'Icy-MetaData: 1',
+				'user_agent' => $ua
+			]
+		];
+		if (($headers = get_headers($streamingUrl)))
+			foreach ($headers as $h)
+				if (strpos(strtolower($h), 'icy-metaint') !== false && ($interval = explode(':', $h)[1]))
+					break;
+		$context = stream_context_create($opts);
+		if ($stream = fopen($streamingUrl, 'r', false, $context))
+		{
+			while($buffer = stream_get_contents($stream, $interval, $offset)) {
+				if (strpos($buffer, $needle) !== false)
+				{
+					fclose($stream);
+					$title = explode($needle, $buffer)[1];
+					return substr($title, 1, strpos($title, ';') - 2);
+				}
+				
+				$offset += $interval;
+			}
+		}
+	}
+?>
